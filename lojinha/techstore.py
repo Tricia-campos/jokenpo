edf806a5810bc4c987b691041aa7235b88dcc083
Tricia-campos <triciacamposm@gmail.com>
2024-09-08 08:14:33 -0300

    loja techstore, produtos disponiveis

---
+++ lojinha/techstore.py
@@ -0,0 +1,24 @@
+
+notbook = ["macbook air", "galaxy book4", "asus vivobook"]
+valornb =["6300.00", "2768.67","2769.89"]
+smartfones = ["iphone 15 pro max", "S23 ultra","moto edge 40" ]
+valorsf =["9967.89", "4200.78", "4090.76"]
+fone = ["airpods pro 3", "galaxy buds fe", "jbl"]
+valorf =["1400.90","469.89","290.60"]
+
+
+print("TECHSTORE")
+print ("""--------------------------------------------------------------
+| confira nossas promoções do dia:                           |
+|NOTBOOKS -> macbook air de 6999.90R$ por 6300.00R$          |
+|            galaxy book4 de 3999.90R$ por 2768.67R$         |
+|            asus vivobook de 2999.90R$ por 2769.89R$        |
+|SMARTONES -> iphone 15 pro max de 9999.90R$ por 9967.89R$   |
+|            S23 ultra de 4399.90R$ por 4200.78R$            |
+|            moto edge 40 de 4299.90R$ por 4090.76R$         |
+|FONE DE OUVIDO -> airpods pro 3 de 1499.90R$ por 1400.90R$  |
+|            galaxy buds fe de 500.90R$ por 469.89R$         |
+|            JBL de 299.90R$ por 290.60R$                    |
+--------------------------------------------------------------""")                
+
+
