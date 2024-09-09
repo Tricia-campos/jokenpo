d93a2e28dcbefe2f473ed393e4f844ae9d2277d7
Tricia-campos <triciacamposm@gmail.com>
2024-09-09 17:09:23 -0300

    alteracoes

--- lojinha/techstore.py
+++ lojinha/techstore.py
@@ -8,17 +8,20 @@ valorf =["1400.90","469.89","290.60"]
 
 
 print("TECHSTORE")
-print ("""--------------------------------------------------------------
-| confira nossas promoções do dia:                           |
-|NOTBOOKS -> macbook air de 6999.90R$ por 6300.00R$          |
-|            galaxy book4 de 3999.90R$ por 2768.67R$         |
-|            asus vivobook de 2999.90R$ por 2769.89R$        |
-|SMARTONES -> iphone 15 pro max de 9999.90R$ por 9967.89R$   |
-|            S23 ultra de 4399.90R$ por 4200.78R$            |
-|            moto edge 40 de 4299.90R$ por 4090.76R$         |
-|FONE DE OUVIDO -> airpods pro 3 de 1499.90R$ por 1400.90R$  |
-|            galaxy buds fe de 500.90R$ por 469.89R$         |
-|            JBL de 299.90R$ por 290.60R$                    |
---------------------------------------------------------------""")                
-
+cliente = input("Bem vindo a techstore, deseja conhecer nossas promoções? (sim/nao): ")
+if cliente == "sim":
+    print ("""   --------------------------------------------------------------
+    | confira nossas promoções do dia:                           |
+    |NOTBOOKS -> macbook air de 6999.90R$ por 6300.00R$          |
+    |            galaxy book4 de 3999.90R$ por 2768.67R$         |
+    |            asus vivobook de 2999.90R$ por 2769.89R$        |
+    |SMARTONES -> iphone 15 pro max de 9999.90R$ por 9967.89R$   |
+    |            S23 ultra de 4399.90R$ por 4200.78R$            |
+    |            moto edge 40 de 4299.90R$ por 4090.76R$         |
+    |FONE DE OUVIDO -> airpods pro 3 de 1499.90R$ por 1400.90R$  |
+    |            galaxy buds fe de 500.90R$ por 469.89R$         |
+    |            JBL de 299.90R$ por 290.60R$                    |
+    --------------------------------------------------------------""")                
+else:
+    print("Até a proxima!")
 
